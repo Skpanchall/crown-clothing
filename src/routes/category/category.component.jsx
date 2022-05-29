e34523c040bcd3f504d1e05911b67800a6760faf
sachin <panchalsachin989845@gmail.com>
2022-05-29 16:24:58 +0530

    for changes

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,7 +2,7 @@ import { useEffect, useContext, useState, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import Productcard from "../../Components/product-card/product-card.component";
 import { Categoriescontext } from "../../contexts/catrgories.contexts";
-import './category.styles.jsx'
+import {CategoryContainer,Title} from  './category.styles.jsx'
 const Category = ()=>{
     const {category} = useParams();
     const {categories} = useContext(Categoriescontext);
@@ -14,14 +14,14 @@ const Category = ()=>{
 
     return(
         <Fragment>
-            <h2 className="category-title">{category}</h2>
-            <div className="category-container">
+            <Title>{category}</Title>
+            <CategoryContainer>
                 {products &&
                     products.map((product)=> {
                     return <Productcard key={product.id} product={product} />
                     })
                 }
-            </div>    
+            </CategoryContainer>    
         </Fragment>
     )
 
